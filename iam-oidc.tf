c32f2bc6ad92f1d503f555dc1ae75f9b5c01cee2
Carlos Carvalho <carlos@chcdc.com.br>
2024-08-13 14:59:13 -0300

    feat:Cria role oidc

--- iam-oidc.tf
+++ iam-oidc.tf
@@ -1,12 +1,10 @@
-# data "tls_certificate" "eks" {
-#  source = "main"
-#  name   = "eks-cluster"
-#  url    = aws_eks_cluster.eks-cluster.identity[0].oidc[0].issuer
-#}
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
 
-#resource "aws_iam_openid_connect_provider" "eks" {
-#  client_id_list  = ["sts.amazonaws.com"]
-#  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
-#  url             = aws_eks_cluster.demo.identity[0].oidc[0].issuer
-#:W}
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
 
