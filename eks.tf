a1cd59d8848b6dee971ed12a6249b5a9f9978baf
Carlos Carvalho <carlos@chcdc.com.br>
2024-08-13 14:58:53 -0300

    feat:Cria nova config para o access_config e access_entry

--- eks.tf
+++ eks.tf
@@ -8,5 +8,29 @@ resource "aws_eks_cluster" "this" {
       aws_subnet.this_public.id
     ]
   }
-  depends_on = [aws_iam_role_policy_attachment.AmazonEKSClusterPolicy]
+
+  access_config {
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.nodes-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.nodes-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.nodes-AmazonEKSWorkerNodePolicy,
+  ]
 }
+
+resource "aws_eks_access_entry" "example" {
+  cluster_name  = aws_eks_cluster.this.name
+  principal_arn = aws_iam_role.this.arn
+  type          = "STANDARD"
+}
+
+# resource "aws_eks_addon" "this" {
+#  cluster_name = aws_eks_cluster.this.name
+#  addon_name   = "vpc-cni"
+#}
+
+
