42083b588df429b7ceebfcdc70da2c7f94152e59
Carlos Carvalho <carlos@chcdc.com.br>
2024-08-13 14:58:18 -0300

    chore:Insere novas saidas de info do kubernetes criado

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,15 @@ output "cluster_id" {
   value       = aws_eks_cluster.this.cluster_id
 }
 
+output "endpoint" {
+  value = aws_eks_cluster.this.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.this.certificate_authority[0].data
+}
+
+
 #output "kubeconfig" {
 #  description = "O kubeconfig do cluster EKS"
 #  value       = module.eks.kubeconfig
